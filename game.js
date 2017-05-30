ed0ee98465c8fdbcf18062a3225b81eb93577da8
Claudia El Khatib <claudiacv22@gmail.com>
2017-05-30 17:14:59 -0400

    html divisions and images

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
-var userChoice = prompt("Do you choose rock, paper or scissors?");
+// var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
 if (computerChoice < 0.34) {
 	computerChoice = "rock";
-} else if(computerChoice >= 0.34 && computerChoice < 0.66) {
+} else if( computerChoice < 0.66) {
 	computerChoice = "paper";
 } else {
 	computerChoice = "scissors";
